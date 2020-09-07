143683cc381704e6534f2b9e706e13d95a1720fb
Niko Makridakis <nmak345@gmail.com>
2020-09-07 10:59:36 -0500

    added DbContext

---
+++ src/Infrastructure/Data/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+    class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+
+    }
+}
