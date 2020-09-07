eed949706e77f54fa1c1a6b5d593ff05c6448086
Niko Makridakis <nmak345@gmail.com>
2020-09-07 10:05:40 -0500

    added stocks controller

---
+++ src/Web/Controllers/StocksController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StocksController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStocks()
+        {
+            return "this will return a list of stocks";
+        }
+
+        [HttpGet("{id}")]
+        public string GetStock(int id)
+        {
+            return "this will return one stock";
+        }
+    }
+}
