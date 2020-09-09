5e865d7ecd76c2c5f1c667371b7759b5caa257c8
Niko Makridakis <nmak345@gmail.com>
2020-09-09 07:15:00 -0500

    created StockRepository

---
+++ src/Infrastructure/Data/StockRepository.cs
@@ -0,0 +1,20 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    class StockRepository : IStockRepository
+    {
+        public Task<Stock> GetStockByIdAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Stock>> GetStocksAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
