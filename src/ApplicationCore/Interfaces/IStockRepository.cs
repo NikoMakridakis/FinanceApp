1227019ca7b4d61308428ac129306a15cf466c86
Niko Makridakis <nmak345@gmail.com>
2020-09-09 07:10:21 -0500

    created IStockRepository

---
+++ src/ApplicationCore/Interfaces/IStockRepository.cs
@@ -0,0 +1,12 @@
+﻿using ApplicationCore.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IStockRepository
+    {
+        Task<Stock> GetStockByIdAsync(int id);
+        Task<IReadOnlyList<Stock>> GetStocksAsync();
+    }
+}
