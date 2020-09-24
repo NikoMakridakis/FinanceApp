8a9d66373ded2db8202d2a62fe378915ba58bd68
Niko Makridakis <nmak345@gmail.com>
2020-09-24 07:27:51 -0500

    unit test

--- tests/UnitTests/Web/Controller/BudgetControllerTests.cs
+++ tests/UnitTests/Web/Controller/BudgetControllerTests.cs
@@ -1,6 +1,43 @@
-﻿namespace UnitTests.Web.Controller
+﻿using AutoMapper;
+using Core.Entities;
+using Core.Interfaces;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Web.Controllers;
+using Web.Models;
+using Xunit;
+
+namespace UnitTests.Web.Controller
 {
-    class BudgetControllerTests
+    public class BudgetControllerTests
     {
+        private readonly BudgetController _budgetController;
+        private readonly Mock<IFinanceAppRepository> _financeAppRepoMock = new Mock<IFinanceAppRepository>();
+        private readonly IMapper _mapper;
+        public BudgetControllerTests()
+        {
+            _budgetController = new BudgetController(_financeAppRepoMock.Object, _mapper);
+        }
+
+
+        [Fact]
+        public async Task GetBudgets_ShouldReturnAllBudgets()
+        {
+            // Arrange
+            var budgetId = 100;
+            var date = DateTime.Now;
+            var budgetDto = new BudgetDto
+            {
+                BudgetId = budgetId,
+                Date = date
+            };
+            _financeAppRepoMock.Setup(x => x.GetBudgetsAsync()).ReturnsAsync();
+
+            // Act
+            await _budgetController.GetBudgets();
+
+            // Assert
+        }
     }
 }
