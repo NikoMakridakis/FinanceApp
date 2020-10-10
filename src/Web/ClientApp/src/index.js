f538ef73996dd26dbb3598592184c4bdd4ca7b93
Niko Makridakis <nmak345@gmail.com>
2020-10-10 15:55:57 -0500

    client pages started

--- src/Web/ClientApp/src/index.js
+++ src/Web/ClientApp/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Home from './components/Home';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Home />,
+    document.getElementById('root')
 );
