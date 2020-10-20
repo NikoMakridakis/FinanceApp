7d6363b6bfc7dbbe9b3c2122bb7cab39f9c54118
Niko Makridakis <nmak345@outlook.com>
2020-10-19 22:28:19 -0500

    login page added

---
+++ src/Web/ClientApp/src/components/Copyright.js
@@ -0,0 +1,32 @@
+﻿import React from 'react';
+import { useHistory } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+
+const useStyles = makeStyles((theme) => ({
+    websiteLink: {
+        cursor: 'pointer'
+    }
+}));
+
+function Copyright() {
+
+    const classes = useStyles();
+
+    const history = useHistory();
+
+    const navigateToHome = () => history.push('/home');
+
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" onClick={navigateToHome} className={classes.websiteLink}>
+                StarBudget
+            </Link>{' '}
+            {new Date().getFullYear()}
+        </Typography>
+    );
+}
+
+export default Copyright;
--- src/Web/ClientApp/src/components/Login.js
+++ src/Web/ClientApp/src/components/Login.js
@@ -1,6 +1,45 @@
-﻿import React, { useState } from "react";
+﻿import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
+import Copyright from './Copyright';
 import AuthService from "../services/AuthService";
+
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+    forgotPassword: {
+        textAlign: 'right',
+        margin: 'auto',
+    },
+}));
 
 function Login(props) {
 
@@ -26,37 +65,82 @@ function Login(props) {
         );
     }
 
+    const classes = useStyles();
+
     return (
-        <div>
-            <div>
-                <form onSubmit={handleLogin}>
-                    <div>
-                        <label htmlFor="email">Email</label>
-                        <input
-                            type="text"
-                            name="email"
-                            value={email}
-                            onChange={onChangeEmail}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            name="password"
-                            value={password}
-                            onChange={onChangePassword}
-                        />
-                    </div>
-                    <div>
-                        <button>
-                            <span>Login</span>
-                        </button>
-                    </div>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <form onSubmit={handleLogin} className={classes.form} noValidate>
+                    <TextField
+                        onChange={onChangeEmail}
+                        value={email}
+                        variant="outlined"
+                        margin="normal"
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        onChange={onChangePassword}
+                        value={password}
+                        variant="outlined"
+                        margin="normal"
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <Grid container>
+                        <Grid item xs>
+                            <FormControlLabel
+                                control={<Checkbox value="remember" color="primary" />}
+                                label="Remember me"
+                            />
+                        </Grid>
+                        <Grid item xs className={classes.forgotPassword}>
+                            <Link href="#" variant="body2">
+                                <Typography>
+                                    Forgot password?
+                                </Typography>
+                            </Link>
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Sign In
+                    </Button>
+                    <Box>
+                        <Typography align="center" variant="body1">
+                            { "Don't have an account? " }
+                            <Link href="#" variant="body1">
+                                    {"Sign Up"}
+                            </Link>
+                        </Typography>
+                    </Box>
                 </form>
             </div>
-        </div>
+            <Box mt={8}>
+                <Copyright />
+            </Box>
+        </Container>
     );
-};
+}
 
 export default Login;
