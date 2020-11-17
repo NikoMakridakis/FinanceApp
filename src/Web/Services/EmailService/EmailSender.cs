5932773000a035ac2c044752fbe6d87415b73f39
Niko Makridakis <nmak345@gmail.com>
2020-11-17 06:33:25 -0600

    created HtmlBody for EmailSender for password reset

--- src/Web/Services/EmailService/EmailSender.cs
+++ src/Web/Services/EmailService/EmailSender.cs
@@ -1,5 +1,6 @@
 ﻿using MailKit.Net.Smtp;
 using MimeKit;
+using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -37,38 +38,48 @@ namespace Web.Services.EmailService
             emailMessage.Subject = message.Subject;
 
             var bodyBuilder = new BodyBuilder {
-                HtmlBody = string.Format($"" +
-                "<div style = 'background-color:#F6F9FC; font-family:Roboto, Helvetica, Arial; padding-top:30px; padding-bottom:30px'>" +
-                    "<div style = 'background-color:#ffffff; color:#525f7f; font-size: 16px; max-width: 600px; margin: 0 auto; padding: 20px 60px 20px 60px;'>" +
-                        "<h1>" +
-                            "<a href='https://localhost:44387/' target='_blank' rel='noopener noreferrer' style='text-decoration:none; color:#1976d2;'>" +
+                HtmlBody = string.Format(
+                    "<div style='background-color: #f6f9fc;'>" +
+                        "<div style='background-color: #ffffff; color: #525f7f; font-family: Roboto, Helvetica, Arial; font-size: 16px; margin: 25px auto; max-width: 600px;'>" +
+                            "<h1 style='padding: 20px 40px; margin: 0px;'>" +
+                                "<a href = 'https://localhost:44387/' target = '_blank' rel = 'noopener noreferrer' style='color: #1976d2; text-decoration: none;'>" +
+                                    "StarBudget" +
+                                "</a>" +
+                            "</h1>" +
+                            "<hr style='background-color: #525f7f; border: none; color: #525f7f; height: 1px; margin: 0px 40px; opacity: 0.15;'/>" +
+                            "<div style='padding: 20px 40px;'>" +
+                                "<p style='font-size: 16px; font-weight: 400px; margin: 0px; padding-bottom: 20px;'>" +
+                                    "Hello," +
+                                "</p>" +
+                                "<p style='display: inline; font-size: 16px; font-weight: 400px;'>" +
+                                    "We received a request to reset the password for the StarBudget account associated with " +
+                                "</p>" +
+                                $"{emailMessage.To}" +
+                                "<p style='display: inline; font-size: 16px; font-weight: 400px;'>.</ p >" +
+                            "</div>" +
+                            "<div style='padding: 20px 40px; text-align: center;'>" +
+                                $"<a href='{message.Content}' style='background-color: #1975d2; border-radius: 8px; color: #f6f9fc; cursor: pointer; display: inline-block; font-size: 16px; font-weight: bold; padding: 13px 40px; text-decoration: none;'>" +
+                                    "Reset your password" +
+                                "</a>" +
+                            "</div>" +
+                            "<div style='padding: 20px 40px;'>" +
+                            "<p style='font-size: 16px; font-weight: 400px; margin: 0px; padding-bottom: 20px;'>" +
+                                "If you didn’t request to reset your password, let us know by replying directly to this email. No changes were made to your account yet." +
+                            "</p>" +
+                            "<p style='font-size: 16px; font-weight: 400px; margin: 0px; padding-bottom: 20px;'>" +
+                                "Thanks," +
+                            "</p>" +
+                            "<p style='font-size: 16px; font-weight: 400px; margin: 0px;'>" +
                                 "StarBudget" +
-                            "</a>" +
-                        "</h1>" +
-                        "<hr style='height:1px; border:none; color:#525f7f; background-color:#525f7f; opacity:0.3;' />" +
-                        "<p style='font-size:16px; font-weight:400px;'>" +
-                            "Hello, <br /><br />" +
-                            "We received a request to reset the password for the Stripe account associated with" +
-                        $"</p>{emailMessage.To}" + "<p>.</p>" +
-                        "<div style='text-align:center; margin-top:30px; margin-bottom:30px;'>" +
-                            $"<a href='{message.Content}' style='background-color:#1975d2; border-radius:8px; display:inline-block; cursor:pointer; color:#f6f9fc; font-size:16px; font-weight:bold; padding:13px 40px; text-decoration:none;'>" +
-                                "Reset your password" +
-                            "</a>" +
+                            "</p>" +
                         "</div>" +
-                        "<p style='font-size:16px; font-weight:400px;'>" +
-                            "If you didn’t request to reset your password, let us know by replying directly to this email. No changes were made to your account yet." +
-                            "<br /><br />" +
-                            "Thanks," +
-                            "<br /><br />" +
-                            "StarBudget" +
-                        "</p>" +
-                        "<hr style='height:1px; border:none; color:#525f7f; background-color:#525f7f; opacity:0.3;' />" +
-                        "<p style='font-size:12px; color:#8898aa'>" +
-                            "Copyright © StarBudget" +
+                        "<hr style='background-color: #525f7f; border: none; color: #525f7f; height: 1px; margin: 0px 40px; opacity: 0.15;'/>" +
+                        "<p style='color: #8898aa; font-size: 12px; margin: 0px; padding: 20px 40px;'>" +
+                            "Copyright © StarBudget " + $"{DateTime.Now.Year}" +
                         "</p>" +
                     "</div>" +
-                "</div>" +
-                $"") };
+                "</div>"
+                ) };
 
             emailMessage.Body = bodyBuilder.ToMessageBody();
             return emailMessage;
